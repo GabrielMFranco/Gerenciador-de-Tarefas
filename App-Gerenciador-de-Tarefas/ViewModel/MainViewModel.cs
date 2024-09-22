5b95b978154781eca2e38d0f9af7faff26647e16
GabrielMFranco <gabri.m.franco@gmail.com>
2024-09-22 18:11:37 -0300

    arrumando botão fora do expander.header

--- App-Gerenciador-de-Tarefas/ViewModel/MainViewModel.cs
+++ App-Gerenciador-de-Tarefas/ViewModel/MainViewModel.cs
@@ -48,11 +48,32 @@ namespace GerenciadorDeTarefas.ViewModel {
 			}
 		}
 		private void RemoveCommand(ToDoItem tarefa) {
-			if (tarefa != null && Tasks.Contains(tarefa)) {
+			if (tarefa != null) {
 				Tasks.Remove(tarefa);
 			}
 		}
 
-		
+		public class RelayCommand<T> : ICommand {
+			private readonly Action<T> _execute;
+			private readonly Predicate<T> _canExecute;
+
+			public RelayCommand(Action<T> execute, Predicate<T> canExecute = null) {
+				_execute = execute ?? throw new ArgumentNullException(nameof(execute));
+				_canExecute = canExecute;
+			}
+
+			public event EventHandler CanExecuteChanged {
+				add { CommandManager.RequerySuggested += value; }
+				remove { CommandManager.RequerySuggested -= value; }
+			}
+
+			public bool CanExecute(object parameter) {
+				return _canExecute == null || _canExecute((T)parameter);
+			}
+
+			public void Execute(object parameter) {
+				_execute((T)parameter);
+			}
+		}
 	}
 }
