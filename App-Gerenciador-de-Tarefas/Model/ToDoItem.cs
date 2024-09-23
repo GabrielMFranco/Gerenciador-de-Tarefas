2dc35eebef6b2dbe4d59d0e15c3d3dc512ed1529
GabrielMFranco <gabri.m.franco@gmail.com>
2024-09-23 11:47:48 -0300

    comecei a parte do checkbox
    referenciei o binding do ckeckbox no expander.style para alterar a cor do backgroud de acordo com o estado do ckeckbox.
    adicionei o comportamento do ckeckbox no ToDoItem e implementei o INPC para notificar a mudança nas propriedades

--- App-Gerenciador-de-Tarefas/Model/ToDoItem.cs
+++ App-Gerenciador-de-Tarefas/Model/ToDoItem.cs
@@ -1,8 +1,38 @@
-﻿namespace GerenciadorDeTarefas.Model {
-	public class ToDoItem {
+﻿using App_Gerenciador_de_Tarefas.Foundation;
+using System.ComponentModel;
+
+namespace GerenciadorDeTarefas.Model {
+	public class ToDoItem : MyObservableObject {
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public string Create { get; set; }
 		public string Completed { get; set; }
+
+		private bool _isCompleted;
+		public bool IsCompleted {
+			get => _isCompleted;
+			set {
+				Set(ref _isCompleted, value, nameof(IsCompleted));
+				OnPropertyChanged(nameof(IsCompleted));
+			}
+		}
+	}
+
+	public class MyObservableObject : INotifyPropertyChanged {
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool Set<T>(ref T field, T value, string propertyName) {
+			if (!EqualityComparer<T>.Default.Equals(field, value)) {
+				field = value;
+				OnPropertyChanged(propertyName);
+				return true;
+			}
+			return false;
+		}
+
+		protected void OnPropertyChanged(string propertyName) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
+
