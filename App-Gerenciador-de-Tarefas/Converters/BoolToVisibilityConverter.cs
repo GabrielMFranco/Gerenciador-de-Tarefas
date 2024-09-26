7491fc4923ad35500bc898cafae2928bc3a17543
GabrielMFranco <gabri.m.franco@gmail.com>
2024-09-26 09:36:42 -0300

    alterado os nomes dos converts

--- App-Gerenciador-de-Tarefas/Converters/BoolToVisibilityConverter.cs
+++ App-Gerenciador-de-Tarefas/Converters/BoolToVisibilityConverter.cs
@@ -11,10 +11,12 @@ using System.Windows.Data;
 
 namespace App_Gerenciador_de_Tarefas.Converters;
 
-public class SaveIndicatorVisibility : IValueConverter {
-	public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-		return (bool)value ? Visibility.Visible : Visibility.Collapsed;
-	}
+public class BoolToVisibilityConverter : IValueConverter {
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
+		=> value is bool val && val
+			? Visibility.Visible 
+			: Visibility.Collapsed;
+	
 	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
 		throw new NotImplementedException(); 
 	}
